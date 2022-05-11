0ef9cc9cf21bae18dd05b010be821ac5f9e6f16f
A.D_codes <86753228+ImArnoldDCODES@users.noreply.github.com>
2022-05-11 15:18:19 -0700

    Add files via upload
    guild-clone

---
+++ index.js
@@ -0,0 +1,15 @@
+// var con = document.getElementById('con');
+// var ul = document.getElementsByTagName('nav ul');
+// con.addEventListener('click', show);
+
+// if (condition) {
+//     ul.style.display = 'none'
+    
+// }
+
+// function show() {
+//     if (ul.style.display='none') {
+//         // ul.style.display = 'none';
+//         con.style.display = 'nonw'
+//     }
+// }
